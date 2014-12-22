0e2c4ab39562ce516459844bd44eb50a8b424b75
denten <denten@gmail.com>
2014-12-21 19:22:54 -0500

    minor moving around

---
+++ code/progress/progress.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+src=/home/denten/gDrive/papers/projects/workbench/plain-text/main.md
+target=/home/denten/gDrive/code/web/xpmethod.github.io/_includes/plain-text-prog.txt
+
+# use < to display counts only, otherwise wc includes the file name
+w="$(wc -w < $src)"
+stamp="$(date --iso-8601)"
+
+# use -n option to omit the trailing newline
+echo $w >> words.csv
+
+echo -n "word count: " > $target
+cat words.csv | spark >> $target
+echo $w >> $target
+echo "on " >> $target
+echo $stamp >> $target
+
